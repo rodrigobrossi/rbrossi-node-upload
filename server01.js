538af60695753b38a2ad655aefeb6948c7e324bf
Rodrigo Brossi <rbrossi@br.ibm.com>
2018-03-22 09:35:34 -0300

    Test git -reset

--- server01.js
+++ server01.js
@@ -1,9 +1,13 @@
 var http = require('http') //How we require modules
 
+/**
+ * Create a server on git
+ * 
+ */
 http.createServer( function(request,response){
-    response.writeHead(200); //Set the status code in header
-    response.write("Hello, this is Brossi"); //Response Body
-    response.end(); //Close the connection
-}).listen(8088,function(){
+        response.writeHead(200); //Set the status code in header
+        response.write("Hello, this is Brossi"); //Response Body
+        response.end(); //Close the connection
+    }).listen(8088,function(){
     console.log('Listening on port 8088...');
 });
