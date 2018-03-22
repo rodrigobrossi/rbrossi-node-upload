0b41ac91fd6bcefdade76b3f39862434b8fb8f5e
Rodrigo Brossi <rbrossi@br.ibm.com>
2018-03-22 09:39:29 -0300

    Add text for a second file

--- server02.js
+++ server02.js
@@ -1,5 +1,9 @@
 var http = require('http') //How we require modules
 
+/**
+ * This is a improvement from the first version
+ * 
+ */
 http.createServer( function(request,response){
     response.writeHead(200); //Set the status code in header
     response.write("Dog is running"); //Response Body
